88fbeca4b2eb3b7d77b7389dcd761dbeda05421f
Nikhil22 <nikhil38@hotmail.com>
2021-08-11 21:50:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import MarketStructure
 class Killshots(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2020, 2, 1)
-        self.SetEndDate(2020, 2, 28)
+        self.SetStartDate(2020, 3, 1)
+        self.SetEndDate(2020, 3, 31)
         self.SetCash(24000) 
         self.AddUniverseSelection(KSUniverseModel())
-        self.UniverseSettings.Resolution = Resolution.Minute
+        self.UniverseSettings.Resolution = Resolution.Hour
         self.riskPerTrade = 0.005
         self.minCash = self.Portfolio.Cash / 2
         self.DefaultOrderProperties.TimeInForce = TimeInForce.Day
+        self.theoreticalPortfolioCash = self.Portfolio.Cash
+        self.Schedule.On(self.DateRules.EveryDay(), self.TimeRules.At(0, 0), self.ResetTheoreticalPortfolioCash)
         
     def OnData(self, slice):
         for key in slice.Bars:
@@ -25,13 +27,14 @@ class Killshots(QCAlgorithm):
             )
             if currentPrice <= stopLossPrice:
                 continue
-            if self.Portfolio.Cash - (entryPrice * quantity) <= self.minCash:
+            if self.theoreticalPortfolioCash - (entryPrice * quantity) <= self.minCash:
                 continue
             if len(self.Transactions.GetOpenOrders(symbol)) > 0:
                 continue
             if self.Securities[symbol].Invested:
                 continue
             self.LimitOrder(symbol, quantity, entryPrice)
+            self.theoreticalPortfolioCash -= (entryPrice * quantity)
             SharedState.addToSymbolsWithOpenOrders(symbol)
 
         # Take profit, stop loss
@@ -48,3 +51,6 @@ class Killshots(QCAlgorithm):
                     continue
                 if close <= SharedState.getStopLossPrice(symbol):
                     self.Liquidate(symbol)
+
+    def ResetTheoreticalPortfolioCash(self):
+        self.theoreticalPortfolioCash = self.Portfolio.Cash
