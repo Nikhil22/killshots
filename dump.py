340ce524bb66ff6f3d1e8e56bf9ffdf589908bc9
Nikhil22 <nikhil38@hotmail.com>
2021-08-12 20:13:24 -0600

    Update dump.py

--- dump.py
+++ dump.py
@@ -12,3 +12,31 @@
 #         ):
 #         #cancel all orders
 #         self.Transactions.CancelOpenOrders(symbol)
+
+# # Sell half
+# quantity = self.Portfolio[symbol].Quantity
+# quantityToSell = math.floor(quantity / 2) * -1
+# self.MarketOrder(symbol, quantityToSell)
+
+# #Create trailing stop
+# self.StopMarketOrder(symbol, quantityToSell, 0.85 * close)
+                    
+# for symbol, history in historySlices.groupby(level=0):
+#     if symbol not in self.averages:
+#         self.averages[symbol] = SymbolData(symbol, history)
+#     else:
+#         avg = self.averages[symbol]
+#         latestTime = history.loc[symbol].index[-1]
+#         lastClose = history.loc[symbol].close.iloc[-1]
+#         lastVolume = history.loc[symbol].volume.iloc[-1]
+#         avg.updatePriceAvgs(latestTime, lastClose)
+#         # avg.updateVolumeAvgs(latestTime, lastVolume)
+
+#     SharedState.updateHistory(symbol, history)
+
+# def passesAverages(x):
+#     return (
+#         x.isUptrend() 
+#         # and x.hasSufficientVolume(MIN_VOLUME)
+#     )
+# initialFilter2 = list(filter(lambda x: passesAverages(x), self.averages.values()))
