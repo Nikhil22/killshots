a65e1d6866e1239b6b0ed704de9170249d2ffd7d
Nikhil22 <nikhil38@hotmail.com>
2021-08-13 21:57:14 -0600

    Add logic to not trade stopped out symbols

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import math
 class Killshots(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2021, 1, 1)
-        # self.SetEndDate(2020, 12, 31)
+        self.SetStartDate(2015, 1, 1)
+        self.SetEndDate(2015, 12, 31)
         self.SetCash(24000) 
         self.AddUniverseSelection(KSUniverseModel())
         self.UniverseSettings.Resolution = Resolution.Minute
@@ -31,6 +31,8 @@ class Killshots(QCAlgorithm):
             if self.Securities[symbol].Invested:
                 continue
             if currentPrice > entryPrice:
+                continue
+            if SharedState.isStoppedOutSymbol(symbol):
                 continue
             self.MarketOrder(symbol, quantity)
             holdings = self.Portfolio[symbol].Quantity
@@ -57,6 +59,7 @@ class Killshots(QCAlgorithm):
                     trailingStopTicket.Update(updateFields)
                 
                 if close <= SharedState.getStopLossPrice(symbol):
+                    SharedState.addStoppedOutSymbol(symbol)
                     self.Liquidate(symbol)
                     
     def OnOrderEvent(self, orderEvent):
@@ -71,7 +74,7 @@ class Killshots(QCAlgorithm):
         if takeProfitOrderTicket is not None and order.Id == takeProfitOrderTicket.OrderId:
             holdings = self.Portfolio[symbol].Quantity
             fillPrice = orderEvent.FillPrice
-            ticket = self.StopMarketOrder(symbol, -holdings, 0.9 * fillPrice)
+            ticket = self.StopMarketOrder(symbol, -holdings, 0.8 * fillPrice)
             SharedState.setTrailingStopOrderTicket(symbol, ticket)
             SharedState.setHighestPrice(symbol, fillPrice)
             return
@@ -80,3 +83,9 @@ class Killshots(QCAlgorithm):
             SharedState.removeHighestPrice(symbol)
             SharedState.removeTrailingStopOrderTicket(symbol)
             SharedState.removeTakeProfitOrderTicket(symbol)
+            
+    def OnSecuritiesChanged(self, changes):
+        for x in changes.RemovedSecurities:
+            symbol = x.Symbol
+            if SharedState.isStoppedOutSymbol(symbol):
+                SharedState.removeStoppedOutSymbol(symbol)
